2c48a4e9531ad4bc7a3f4fd0d6eba206209e4cc6
fujin <386520874@qq.com>
2018-10-01 16:30:56 +0800

    1. add Help.cpp

---
+++ FuncRoute/src/Help.cpp
@@ -0,0 +1,23 @@
+#include "Help.h"
+
+
+CHelp::CHelp()
+{
+
+
+}
+
+
+CHelp::~CHelp()
+{
+
+
+}
+
+
+int CHelp::printHelp(int argc, char *argv[])
+{
+
+
+	return 0;
+}
---
+++ FuncRoute/src/Help.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class CHelp
+{
+public:
+
+
+public:
+	CHelp();
+	~CHelp();
+
+	int printHelp(int argc, char *argv[]);
+};
+
